a55ef738603ea86e0f329b410a49cbda84198d32
RoxJade <roxannejade88@icloud.com>
2021-04-15 15:08:48 +0000

    Add categories.html and functionality to manage categories and styled card categories

--- app.py
+++ app.py
@@ -183,6 +183,13 @@ def delete_smoothie(recipe_id):
     return redirect(url_for("get_recipes"))
 
 
+@app.route("/get_categories")
+def get_categories():
+    """Get list of categories from MongoDB and sort them alphabetically"""
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
